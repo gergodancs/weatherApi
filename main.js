4d54e5eb6441a666a255b260ef35d8e85a36caf1
gergodancs <dancs.gergely@gmail.com>
2022-01-06 15:57:14 +0100

    alap

---
+++ main.js
@@ -0,0 +1,24 @@
+// https://www.weatherapi.com
+// apikey 2acf3b0415d041fca8a132314211412
+// apicall for london http://api.weatherapi.com/v1/current.json?key=2acf3b0415d041fca8a132314211412&q=London&aqi=yes
+// reponse
+// "location": {
+//     "name": "London",
+//     "region": "City of London, Greater London",
+//     "country": "United Kingdom",
+//     "lat": 51.52,
+//     "lon": -0.11,
+//     "tz_id": "Europe/London",
+//     "localtime_epoch": 1641480026,
+//     "localtime": "2022-01-06 14:40"
+// },
+// "current": {
+//     "last_updated_epoch": 1641479400,
+//     "last_updated": "2022-01-06 14:30",
+//     "temp_c": 5.0,
+//     "temp_f": 41.0,
+//     "is_day": 1,
+//     "condition": {
+//         "text": "Moderate rain",
+//         "icon": "//cdn.weatherapi.com/weather/64x64/day/302.png",
+//         "code": 1189
